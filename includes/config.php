716cee7913f93e60967dd77ae3d9357ee653f1d6
Hiren Master <hiren@rethinkpay.com>
2025-07-16 17:24:26 +0530

    initial release of a plugin

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php
+// config.php
+if (!defined('BYTENFT_ONRAMP_PROTOCOL')) {
+    define('BYTENFT_ONRAMP_PROTOCOL', is_ssl() ? 'https://' : 'http://');
+}
+
+if (!defined('BYTENFT_ONRAMP_HOST')) {
+    define('BYTENFT_ONRAMP_HOST', 'bytenft.xyz');
+}
+
+if (!defined('BYTENFT_ONRAMP_BASE_URL')) {
+	define('BYTENFT_ONRAMP_BASE_URL', BYTENFT_ONRAMP_PROTOCOL . BYTENFT_ONRAMP_HOST);
+}
