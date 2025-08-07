48feeb9549295874929a4056e4b6f7e904b788ad
Hiren Master <hiren@rethinkpay.com>
2025-08-07 10:31:05 +0530

    update acquire lock logic for better handling of expiry

--- includes/class-bytenft-onramp-payment-gateway-loader.php
+++ includes/class-bytenft-onramp-payment-gateway-loader.php
@@ -184,15 +184,6 @@ class BYTENFT_ONRAMP_PAYMENT_GATEWAY_Loader
 			return new WP_REST_Response(['error' => esc_html__('Order not found', 'bytenft-onramp-payment-gateway')], 404);
 		}
 
-		wc_get_logger()->info(' check payment status fnc response ', [
-				'source'  => 'bytenft-onramp-payment-gateway',
-				'context' => [
-					'order_id'           => $order_id,
-					'order_status' => $order->get_status()
-				],
-			]);
-
-
 		$payment_return_url = esc_url($order->get_checkout_order_received_url());
 
 		// Determine order status
